82edb18f1fdd0bc023cea5e8a8763dd3627cbdde
NicolasCayun <ni.cayun@duocuc.cl>
2025-04-09 21:34:25 -0400

    Modelos

--- vetweb/models.py
+++ vetweb/models.py
@@ -26,6 +26,32 @@ class Producto(models.Model):
 
     def __str__(self):
         return f"{self.nombre} (${self.precio:,})".replace(",", ".")
+    
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('ADMIN', 'Admin'),
+        ('CLIENT', 'Client'),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='CLIENT')
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f'{self.user.username} - {self.role}'
+
+
+# Señal para crear automáticamente el perfil cuando se crea un usuario
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance, role='CLIENT')
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    try:
+        instance.userprofile.save()
+    except UserProfile.DoesNotExist:
+        UserProfile.objects.create(user=instance, role='CLIENT')
 
 class Carrito(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -48,3 +74,43 @@ class CarritoItem(models.Model):
     
     def get_subtotal(self):
         return self.producto.precio * self.cantidad 
+    
+class Orden(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pendiente'),
+        ('COMPLETED', 'Completada'),
+        ('CANCELLED', 'Cancelada'),
+    ]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
+    created_at = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f'Orden {self.id} - {self.user.username}'
+
+class OrdenItem(models.Model):
+    orden = models.ForeignKey(Orden, related_name='items', on_delete=models.CASCADE)
+    producto = models.ForeignKey('Producto', on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField()
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f'{self.cantidad} x {self.producto.nombre}'
+    
+    def get_subtotal(self):
+        return self.precio_unitario * self.cantidad        
+        
+class ListaDeseos(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    fecha_agregado = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['user', 'producto']  # Evita duplicados
+        verbose_name = "Lista de Deseos"
+        verbose_name_plural = "Lista de Deseos"
+
+    def __str__(self):
+        return f'{self.user.username} - {self.producto.nombre}'
